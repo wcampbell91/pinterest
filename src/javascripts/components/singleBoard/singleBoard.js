0eb59f6b4245a47c95d3fb908dc21d368e8c914f
Billy Campbell <wcampbell91@gmail.com>
2020-06-28 19:57:41 -0500

    added a button to take you back to boards page

--- src/javascripts/components/singleBoard/singleBoard.js
+++ src/javascripts/components/singleBoard/singleBoard.js
@@ -1,11 +1,13 @@
 import pinData from '../../helpers/data/pinData';
 import utils from '../../helpers/utils';
+import boards from '../boards/boards';
+import pins from '../pins/pins';
 
 import './singleBoard.scss';
 
 const buildSingleBoard = (e) => {
   const boardId = e.target.closest('.card').id;
-  console.error(boardId);
+  pins.addDiv();
   pinData.getPins()
     .then((response) => {
       const myPins = response;
@@ -15,7 +17,7 @@ const buildSingleBoard = (e) => {
       myPins.forEach((pin) => {
         if (pin.boardId === boardId) {
           domString += `
-                        <div class="card text-center" style="width: 18rem;">
+                        <div class="card pins-card text-center" style="width: 18rem;">
                           <img src="${pin.imageUrl}" class="card-img-top" alt="...">
                           <div class="card-body">
                             <h5 class="card-title">${pin.id}</h5>
@@ -25,9 +27,17 @@ const buildSingleBoard = (e) => {
         }
       });
 
-      domString += '</div>';
+      domString += `</div>
+                    <div class="text-center mt-5">
+                      <button class="btn btn-danger" id="back-button"><i class="fas fa-arrow-left fa-3x"></i></button>
+                    </div>`;
 
       utils.printToDom('#pins', domString);
+
+      $('body').on('click', '#back-button', boards.addDiv);
+      $('body').on('click', '#back-button', pins.removeDiv);
+      $('body').on('click', '.navbar-brand', boards.addDiv);
+      $('body').on('click', '.navbar-brand', pins.removeDiv);
     })
     .catch((err) => console.error('singleBoards broke', err));
 };
