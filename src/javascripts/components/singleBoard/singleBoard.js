5dfcdb289e65641d4114534affa0fd3f46cb67cd
Billy Campbell <wcampbell91@gmail.com>
2020-07-01 21:50:23 -0500

    trying to delete pins!!!

--- src/javascripts/components/singleBoard/singleBoard.js
+++ src/javascripts/components/singleBoard/singleBoard.js
@@ -5,6 +5,16 @@ import pins from '../pins/pins';
 
 import './singleBoard.scss';
 
+const removePinEvent = (e) => {
+  const pinId = e.target.closest('.card').id;
+  pinData.deletePins(pinId)
+    .then(() => {
+      // eslint-disable-next-line no-use-before-define
+      rebuildSingleBoard(e);
+    })
+    .catch((err) => console.error(err));
+};
+
 const buildSingleBoard = (e) => {
   const boardId = e.target.closest('.card').id;
   const boardName = e.target.closest('.card-body').id;
@@ -19,10 +29,11 @@ const buildSingleBoard = (e) => {
       myPins.forEach((pin) => {
         if (pin.boardId === boardId) {
           domString += `
-                        <div class="card pins-card text-center" style="width: 18rem;">
+                        <div class="card pins-card text-center" id="${pin.id}" style="width: 18rem;">
                           <a href="${pin.link}">
                             <img src="${pin.imageUrl}" class="card-img-top pin-image" alt="...">
                           </a>
+                          <button class="btn btn-secondary delete-pin" id="${pin.boardId}">Delete pin</button>
                           <div class="overlay"></div>
                         </div>`;
         }
@@ -34,6 +45,47 @@ const buildSingleBoard = (e) => {
                     </div>`;
 
       utils.printToDom('#pins', domString);
+      $('body').on('click', '.delete-pin', removePinEvent);
+      $('body').on('click', '#back-button', boards.addDiv);
+      $('body').on('click', '#back-button', pins.removeDiv);
+      $('body').on('click', '.navbar-brand', boards.addDiv);
+      $('body').on('click', '.navbar-brand', pins.removeDiv);
+    })
+    .catch((err) => console.error('singleBoards broke', err));
+};
+
+const rebuildSingleBoard = (e) => {
+  console.error(e);
+  const boardId = e.target.closest('.card').id;
+  const boardName = e.target.id;
+
+  pins.addDiv();
+  pinData.getPins()
+    .then((response) => {
+      const myPins = response;
+      let domString = `
+                      <h2 class="text-center mt-4 mb-4">${boardName}</h2>
+                      <div class="d-flex flex-wrap myPins card-deck">`;
+      myPins.forEach((pin) => {
+        if (pin.boardId === boardId) {
+          domString += `
+                        <div class="card pins-card text-center" id="${pin.id}" style="width: 18rem;">
+                          <a href="${pin.link}">
+                            <img src="${pin.imageUrl}" class="card-img-top pin-image" alt="...">
+                          </a>
+                          <button class="btn btn-secondary delete-pin">Delete pin</button>
+                          <div class="overlay"></div>
+                        </div>`;
+        }
+      });
+
+      domString += `</div>
+                    <div class="text-center mt-5">
+                      <button class="btn btn-danger" id="back-button"><i class="fas fa-arrow-left fa-3x"></i></button>
+                    </div>`;
+
+      utils.printToDom('#pins', domString);
+      $('body').on('click', '.delete-pin', removePinEvent);
       $('body').on('click', '#back-button', boards.addDiv);
       $('body').on('click', '#back-button', pins.removeDiv);
       $('body').on('click', '.navbar-brand', boards.addDiv);
