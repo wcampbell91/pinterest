38bc75276bbb5a964ec4d94692400b3952ba75ab
Billy Campbell <wcampbell91@gmail.com>
2020-07-03 16:29:11 -0400

    added a home img / fixed some broken pin links

--- src/javascripts/components/home/home.js
+++ src/javascripts/components/home/home.js
@@ -12,6 +12,9 @@ const homeBuilder = () => {
   const domString = `
   <div class="text-center mt-4">
     <h1>Welcome to Pinterest!</h1>
+    <div id="homeImg">
+      <img id="homeImg" src="https://s.pinimg.com/images/facebook_share_image.png" alt="">
+    </div>
   </div>`;
 
   utils.printToDom('#home', domString);
